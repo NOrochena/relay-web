b193ee59862a57d559887608104526002ab23667
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-09-04 17:24:38 -0400

    Added Apollo. Added User creation and login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/react-hooks';
+
+const CREATE_USER = gql`
+  mutation CreateUser($username: String!, $password: String!) {
+    createUser(username: $username, password: $password) {
+      body
+    }
+  }
+`
+const LOGIN = gql`
+  mutation UserLogin($username: String!, $password: String!) {
+    userLogin(username: $username, password: $password) {
+      body
+    }
+  }
+`
 
 function App() {
+  const [username, setUsername] = useState("")
+  const [password, setpassword] = useState("")
+
+  const [createUser] = useMutation(CREATE_USER, {onCompleted(data){
+    console.log(data)
+  }})
+
+  const [login] = useMutation(LOGIN, {onCompleted(data){
+    console.log(data)
+  }})
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <input value={username} onChange={(e) => setUsername(e.target.value)}/>
+        <input value={password} onChange={(e) => setpassword(e.target.value)}/>
+        <button onClick={() => createUser({variables: {username, password}})}>
+          Create
+        </button>
+        <input value={username} onChange={(e) => setUsername(e.target.value)}/>
+        <input value={password} onChange={(e) => setpassword(e.target.value)}/>
+        <button onClick={() => login({variables: {username, password}})}>
+          Login
+        </button>
+      </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,7 +4,33 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from "apollo-boost"
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { setContext } from 'apollo-link-context'
+
+const httpLink = new HttpLink({
+    uri: 'http://localhost:3000/graphql',
+  });
+  
+  const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem('token')
+    return {
+      headers: {
+        ...headers,
+        Authorization: token ? `Bearer ${token}` : ''
+      }
+    }
+  })  
+  
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
+  })
+
+ReactDOM.render(<ApolloProvider client={client}>
+    <App /></ApolloProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
